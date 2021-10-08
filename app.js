eb40b17134bdadf573cb92ab068c41b47754f68d
AlanGMerkin <alangmerkin@gmail.com>
2021-10-08 15:35:49 -0300

    Cambio de puerto

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(userLoggedMiddleware);
 
 
 /*** PUERTO***/
-app.listen(process.env.PORT || 3001, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo");
 })
 
